caeafd2273eb28a62987181047b59d2451da1787
Stéphane Balaguer <st.balaguer@gmail.com>
2019-06-24 15:01:46 +0200

    Fin de book Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MaBibliotheque';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyB9l0m3KTdU1hKetaTvK7uVytLXLkREL8g",
+      authDomain: "mabibliotheque-6145c.firebaseapp.com",
+      databaseURL: "https://mabibliotheque-6145c.firebaseio.com",
+      projectId: "mabibliotheque-6145c",
+      storageBucket: "",
+      messagingSenderId: "967486330803",
+      appId: "1:967486330803:web:3538aafd05ad55a6"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(config);
+
+  }
+
+
+
 }
